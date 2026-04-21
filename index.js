fc064d1ceec176ba5b0f3d4316a1ea61a3d0b2b8
rmg073 <145686216+rmg073@users.noreply.github.com>
2026-04-21 16:54:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world 🚀");
-});
-
 const PORT = process.env.PORT || 3000;
+
+// 👉 THIS IS REQUIRED
+app.get("/", (req, res) => {
+  res.send("Hello from Railway 🚀");
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
