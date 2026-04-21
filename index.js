02992762c209bd856b515bf36188c71d2d82b83c
rmg073 <145686216+rmg073@users.noreply.github.com>
2026-04-21 16:45:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is working 🚀");
+  res.send("Hello world 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
